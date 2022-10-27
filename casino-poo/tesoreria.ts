c5f7797be0177d4b8cd1a3a4fb194f43cdf27372
= <dariocabrera@tdf.edu.ar>
2022-10-27 20:47:47 -0300

    clase Tesoreria Balance añadido

--- casino-poo/tesoreria.ts
+++ casino-poo/tesoreria.ts
@@ -1,43 +1,22 @@
 export class Tesoreria {
-    private perdida: number;
-    private ganancia: number;
-    private balance: number; //reemplaza a las otras dos?
+    private totalCaja: number; 
     private capitalGeneral: number;
     private valorCreditos: number;
 
     constructor(paramCapital: number, paramValorCreditos: number) {
-        this.perdida = 0;
-        this.ganancia = 0;
-        this.balance = 0;
+        this.totalCaja = 0;
         this.capitalGeneral = paramCapital;
         this.valorCreditos = paramValorCreditos;
-    }
-
-    
-    public getPerdida(): number {
-        return this.perdida;
-    }
-    
-    public setPerdida(paramPerdida: number): void {
-        this.perdida = paramPerdida;
-    }
-    
-    public getGanancia(): number {
-        return this.ganancia;
-    }
-    
-    public setGanancia(paramGanancia: number): void {
-        this.ganancia = paramGanancia;
     }
     
     //se reemplaza a las otras 4 de arriba(?), puede q incluso setBalance tmb desaparezca, porq la resta y suma se hace dentro de otra funcion o dejarla?
 
     public setBalance(paramBalance: number): void {
-        this.balance = paramBalance;
+        this.totalCaja = paramBalance;
     }
 
     public getBalance(): number {
-        return this.balance;
+        return this.totalCaja;
     }
 
     public getCapitalGeneral(): number {
@@ -61,6 +40,7 @@ export class Tesoreria {
     public comprarCreditos(paramDinero: number): number {
         let creditosSalida: number = paramDinero / this.valorCreditos;
         this.capitalGeneral += paramDinero;
+        this.totalCaja += paramDinero;
         return creditosSalida; 
 
     }
@@ -68,6 +48,7 @@ export class Tesoreria {
     public venderCreditos(paramCreditos: number): number {
         let dineroSalida: number = paramCreditos * this.valorCreditos;
         this.capitalGeneral -= dineroSalida;
+        this.totalCaja -= dineroSalida;
         return dineroSalida;
     }
 
