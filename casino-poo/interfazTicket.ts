0a992b92b3ea8f7879d4015145f724c8d701d64a
= <dariocabrera@tdf.edu.ar>
2022-10-27 20:20:19 -0300

    interfaz ticket actualizada

--- casino-poo/interfazTicket.ts
+++ casino-poo/interfazTicket.ts
@@ -1,8 +1,5 @@
 export interface Ticket {
     getCreditos(): number;
-    //getGanancias(): number;
-    //getPerdidas(): number;
-    //reemplaza a Perdida y ganancia
     getBalance(): number;
     getCantidadApuestasTotales(): number;
     getCantidadApuestasGanadas(): number;
