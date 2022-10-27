73efe3b60690b15c278847e812817db4a7e11154
= <dariocabrera@tdf.edu.ar>
2022-10-27 20:42:04 -0300

    añadida apuesta minima opcional

--- casino-poo/juego.ts
+++ casino-poo/juego.ts
@@ -3,20 +3,25 @@ import { Ticket } from "./interfazTicket";
 
 export class Juego implements Ticket {
     protected id : number;
-    //cambio a nombre mAs descriptivo(?)
     protected cantCreditosEnMaquina: number;
-    protected balance : number;
+    protected balance: number;
     protected cantApuestasTotales : number;
     protected cantApuestasGanadas : number;
     protected cantApuestasPerdidas : number;
+    protected cantApuestaMinima: number;
 
-    constructor(paramID: number, paramCreditos: number) {
+    constructor(paramID: number, paramCreditos: number, paramCantApuestaMinima?: number) {
         this.id = paramID;
         this.cantCreditosEnMaquina = paramCreditos;
         this.balance = 0;
         this.cantApuestasTotales = 0;
         this.cantApuestasGanadas = 0;
         this.cantApuestasPerdidas = 0;
+        if(paramCantApuestaMinima === undefined) {
+            this.cantApuestaMinima = 0;
+        } else {
+            this.cantApuestaMinima = paramCantApuestaMinima;
+        }
     }
 
     public ticketTotal(): string[] {
