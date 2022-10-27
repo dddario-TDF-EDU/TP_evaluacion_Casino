ca6182506991f032dc1617ead0b0adf9f8e3af8a
= <dariocabrera@tdf.edu.ar>
2022-10-27 12:13:07 -0300

    cambio minimo interfaz acordado

--- casino-poo/interfazTicket.ts
+++ casino-poo/interfazTicket.ts
@@ -1,7 +1,9 @@
 export interface Ticket {
     getCreditos(): number;
-    getGanancias(): number;
-    getPerdidas(): number;
+    //getGanancias(): number;
+    //getPerdidas(): number;
+    //reemplaza a Perdida y ganancia
+    getBalance(): number;
     getCantidadApuestasTotales(): number;
     getCantidadApuestasGanadas(): number;
     getCantidadApuestasPerdidas(): number;
