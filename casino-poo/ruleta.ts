c00697d0a94b77d49917265f0b546fe4b73ea730
= <dariocabrera@tdf.edu.ar>
2022-10-28 20:29:50 -0300

    Ruleta corregida

--- casino-poo/ruleta.ts
+++ casino-poo/ruleta.ts
@@ -2,42 +2,19 @@ import { Juego } from "./juego";
 
 export class Ruleta extends Juego {
 
-    private primeraDocena: number[];
-    private segundaDocena: number[];
-    private terceraDocena: number[];
-    private primeraColumna: number[];
-    private segundaColumna: number[];
-    private terceraColumna: number[];
-    private primeraMitad: number[];
-    private segundaMitad: number[];
-    private rojas: number[];
-    private negras: number[];
     private resultadoRuleta: number;
-
     
     constructor(paramID: number, paramCreditos: number) {
         super(paramID, paramCreditos);
-        this.primeraDocena = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-        this.segundaDocena = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
-        this.terceraDocena = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
-        this.primeraColumna = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34];
-        this.segundaColumna = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35];
-        this.terceraColumna = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36];
-        this.primeraMitad = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-        this.segundaMitad = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
-        this.rojas = [32, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12, 3];
-        this.negras = [15, 4, 2, 17, 6, 13, 11, 8, 10, 24, 33, 20, 31, 22, 29, 28, 35, 26];
         this.resultadoRuleta = 0;
     }
 
     public apuestaPrimeraDocena(paramCreditos: number): number {
         this.girarRuleta();
         let multiplicador: number = 0;
-        for(let i = 0; i < this.primeraDocena.length; i++) {
-            if(this.primeraDocena[i] === this.resultadoRuleta) {
-                multiplicador = 1.3;
-                return multiplicador * paramCreditos;
-            }
+        if(this.resultadoRuleta < 13 && this.resultadoRuleta != 0) {
+            multiplicador = 1.3;
+            return multiplicador * paramCreditos;
         }
 
         return multiplicador;
@@ -46,11 +23,9 @@ export class Ruleta extends Juego {
     public apuestaSegundaDocena(paramCreditos: number): number {
         this.girarRuleta();
         let multiplicador: number = 0;
-        for(let i = 0; i < this.segundaDocena.length; i++) {
-            if(this.segundaDocena[i] === this.resultadoRuleta) {
-                multiplicador = 1.3;
-                return multiplicador * paramCreditos;
-            }
+        if(this.resultadoRuleta < 25 && this.resultadoRuleta > 12) {
+            multiplicador = 1.3;
+            return multiplicador * paramCreditos;
         }
 
         return multiplicador;
@@ -59,11 +34,9 @@ export class Ruleta extends Juego {
     public apuestaTerceraDocena(paramCreditos: number): number {
         this.girarRuleta();
         let multiplicador: number = 0;
-        for(let i = 0; i < this.terceraDocena.length; i++) {
-            if(this.terceraDocena[i] === this.resultadoRuleta) {
-                multiplicador = 1.3;
-                return multiplicador * paramCreditos;
-            }
+        if(this.resultadoRuleta > 24) {
+            multiplicador = 1.3;
+            return multiplicador * paramCreditos;
         }
 
         return multiplicador;
@@ -72,8 +45,9 @@ export class Ruleta extends Juego {
     public apuestaPrimeraColumna(paramCreditos: number): number {
         this.girarRuleta();
         let multiplicador: number = 0;
-        for(let i = 0; i < this.primeraColumna.length; i++) {
-            if(this.primeraColumna[i] === this.resultadoRuleta) {
+        let primeraColumna: number[] = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34];
+        for(let i = 0; i < primeraColumna.length; i++) {
+            if(primeraColumna[i] === this.resultadoRuleta) {
                 multiplicador = 1.3;
                 return multiplicador * paramCreditos;
             }
@@ -85,8 +59,9 @@ export class Ruleta extends Juego {
     public apuestaSegundaColumna(paramCreditos: number): number {
         this.girarRuleta();
         let multiplicador: number = 0;
-        for(let i = 0; i < this.segundaColumna.length; i++) {
-            if(this.segundaColumna[i] === this.resultadoRuleta) {
+        let segundaColumna: number[] = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35];
+        for(let i = 0; i < segundaColumna.length; i++) {
+            if(segundaColumna[i] === this.resultadoRuleta) {
                 multiplicador = 1.3;
                 return multiplicador * paramCreditos;
             }
@@ -98,8 +73,9 @@ export class Ruleta extends Juego {
     public apuestaTerceraColumna(paramCreditos: number): number {
         this.girarRuleta();
         let multiplicador: number = 0;
-        for(let i = 0; i < this.terceraColumna.length; i++) {
-            if(this.terceraColumna[i] === this.resultadoRuleta) {
+        let terceraColumna: number[] = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36];
+        for(let i = 0; i < terceraColumna.length; i++) {
+            if(terceraColumna[i] === this.resultadoRuleta) {
                 multiplicador = 1.3;
                 return multiplicador * paramCreditos;
             }
@@ -111,11 +87,9 @@ export class Ruleta extends Juego {
     public apuestaPrimeraMitad(paramCreditos: number): number {
         this.girarRuleta();
         let multiplicador: number = 0;
-        for(let i = 0; i < this.primeraMitad.length; i++) {
-            if(this.primeraMitad[i] === this.resultadoRuleta) {
-                multiplicador = 1.2;
-                return multiplicador * paramCreditos;
-            }
+        if(this.resultadoRuleta < 19 && this.resultadoRuleta != 0) {
+            multiplicador = 1.2;
+            return multiplicador * paramCreditos;
         }
 
         return multiplicador;
@@ -124,11 +98,9 @@ export class Ruleta extends Juego {
     public apuestaSegundaMitad(paramCreditos: number): number {
         this.girarRuleta();
         let multiplicador: number = 0;
-        for(let i = 0; i < this.segundaMitad.length; i++) {
-            if(this.segundaMitad[i] === this.resultadoRuleta) {
-                multiplicador = 1.2;
-                return multiplicador * paramCreditos;
-            }
+        if(this.resultadoRuleta > 18) {
+            multiplicador = 1.2;
+            return multiplicador * paramCreditos;
         }
 
         return multiplicador;
@@ -137,8 +109,9 @@ export class Ruleta extends Juego {
     public apuestaRojas(paramCreditos: number): number {
         this.girarRuleta();
         let multiplicador: number = 0;
-        for(let i = 0; i < this.rojas.length; i++) {
-            if(this.rojas[i] === this.resultadoRuleta) {
+        let rojas: number[] = [32, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12, 3];
+        for(let i = 0; i < rojas.length; i++) {
+            if(rojas[i] === this.resultadoRuleta) {
                 multiplicador = 1.2;
                 return multiplicador * paramCreditos;
             }
@@ -150,8 +123,9 @@ export class Ruleta extends Juego {
     public apuestaNegras(paramCreditos: number): number {
         this.girarRuleta();
         let multiplicador: number = 0;
-        for(let i = 0; i < this.negras.length; i++) {
-            if(this.negras[i] === this.resultadoRuleta) {
+        let negras: number[] = [15, 4, 2, 17, 6, 13, 11, 8, 10, 24, 33, 20, 31, 22, 29, 28, 35, 26];
+        for(let i = 0; i < negras.length; i++) {
+            if(negras[i] === this.resultadoRuleta) {
                 multiplicador = 1.2;
                 return multiplicador * paramCreditos;
             }
