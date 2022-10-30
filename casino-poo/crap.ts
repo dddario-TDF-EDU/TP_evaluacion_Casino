18154e0d843d71bcdc02f3ffb9f04bf7dacf4fa8
Moraschetti <moraschettimatias91@gmail.com>
2022-10-30 19:35:12 -0300

    modificando la clase crap

--- casino-poo/crap.ts
+++ casino-poo/crap.ts
@@ -1,6 +1,19 @@
 class Crap {
+     private dado1: number;
+     private dado2: number;
 
-    constructor() {
-
+    constructor(pDado1:number,pDado2:number) {
+        this.dado1 = pDado1;
+        this.dado2 = pDado2;
     }
+    
+    public getDado1() : number {
+        return this.dado1;
+    }
+    
+    public getDado2() : number {
+        return this.dado2;
+    }
+    
+    
 }
