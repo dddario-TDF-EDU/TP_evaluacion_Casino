a259dcd501b33451768efdf8607358ba7ae3ec0b
Moraschetti <moraschettimatias91@gmail.com>
2022-10-31 21:38:35 -0300

    f5 again

--- casino-poo/crap.ts
+++ casino-poo/crap.ts
@@ -14,9 +14,8 @@ export class Crap extends Juego{
     private tirarDados(): void {
         let numeroSalida: number = Math.floor(Math.random() * 12) + 2;
         this.dados = numeroSalida;
-    }
-// 
-    public apuestaSalida(paramCreditos: number): void {
+    } 
+    public apostarSalida(paramCreditos: number): void {
         this.tirarDados();
         if(this.dados == 7 || this.dados == 11) {
             console.log(`el numero es ${this.dados}, avanza a la proxima ronda`);
@@ -24,7 +23,38 @@ export class Crap extends Juego{
             console.log(`el numero es ${this.dados}, crapping out`);
         }else{
              console.log(`el numero es ${this.dados}, tirar nuevamente los dados, y repetir el numero ${this.dados} para continuar`);
+            this.apostarSalidaSegundoIntento(paramCreditos,this.dados);
+            }
+    }
+    public apostarSalidaSegundoIntento(paramCreditos: number,resultadoAnterior:number): void {
+        this.tirarDados();
+        if(this.dados == 11|| this.dados == resultadoAnterior) {
+            console.log(`el numero es ${this.dados}, avanza a la proxima ronda`);
+        }else if(this.dados == 7){
+             console.log(`el numero es ${this.dados}, crapping out`);
         }
     }
-    
+     public apostarPassLine(paramCreditos:number): void{
+        this.tirarDados();
+        if(this.dados == 7 || this.dados == 11) {
+            console.log(`el numero es ${this.dados}, la apuesta gana`);
+        }else if(this.dados == 2 ||this.dados == 3 || this.dados == 12){
+            console.log(`el numero es ${this.dados}, crapping out, la apuesta pierde`);
+        }else{
+             console.log(`el numero es ${this.dados}, tirar nuevamente los dados, y repetir el numero ${this.dados} para continuar`);
+        }
+    }
+    public apostardontPassBar(paramCreditos:number): void{
+        this.tirarDados();
+        if(this.dados == 7 || this.dados == 11) {
+            console.log(`el numero es ${this.dados}, la apuesta pierde`);
+        }if(this.dados == 2 ||this.dados == 3){
+            console.log(`el numero es ${this.dados}, la apuesta gana`);
+        }else if(this.dados == 12){
+            console.log(`el numero es ${this.dados}, es un empate`);
+        }else{
+            console.log(`el numero es ${this.dados}, tirar nuevamente los dados, y repetir el numero ${this.dados} para continuar`);
+        }
+    }
+
 }
