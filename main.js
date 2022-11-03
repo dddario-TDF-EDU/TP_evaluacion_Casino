5e3700dce7c5e099cd3c6f60abeddbbfabcba0b1
= <dariocabrera@tdf.edu.ar>
2022-11-03 20:13:16 -0300

    main a revisar pero instanciado

--- main.js
+++ main.js
@@ -1,27 +1,85 @@
 "use strict";
 exports.__esModule = true;
-var tragamoneda_1 = require("./casino-poo/tragamoneda");
 var tragamonedaSummer_1 = require("./casino-poo/tragamonedaSummer");
-var prueba = new tragamoneda_1.Tragamoneda();
-//
-//let readlineSync = require('readline-sync');
-//let cantCreditos: number = readlineSync.questionInt('Ingrese la cantidad de creditos que va a comprar');
-//let cantApostada: number = readlineSync.questionInt('Ingrese la cantidad de creditos que va a apostar');
-var TragamonedaSummer1 = new tragamonedaSummer_1.TragamonedaSummer();
-//
-// let clientePrueba: Cliente = new Cliente(1000);
-// let resultadoApuesta: number = TragamonedaSummer1.apuesta(cantApostada);
-// console.log("apostamos " + cantApostada + " y obtuvimos: " + resultadoApuesta);
-// cantCreditos= cantCreditos - cantApostada;
-// cantCreditos= cantCreditos + resultadoApuesta;
-// console.log("nuestros creditos totales son: " + cantCreditos);
-function menuTragamonedas(paramCreditos) {
-    var algo = 0;
-    for (var i = 0; i < 10; i++) {
-        console.log("Usted gano: " + TragamonedaSummer1.apuesta(paramCreditos / 10));
-        TragamonedaSummer1.mostrarResultado();
+var cliente_1 = require("./casino-poo/cliente");
+var mainSummer_1 = require("./mainSummer");
+var mainRuleta_1 = require("./mainRuleta");
+var casino_1 = require("./casino-poo/casino");
+var TragamonedaWinter_1 = require("./casino-poo/TragamonedaWinter");
+var ruleta_1 = require("./casino-poo/ruleta");
+var crap_1 = require("./casino-poo/crap");
+//falta resolver la clase Cliente-Tesorersia-Casino para poder probar esto.
+var summer1 = new tragamonedaSummer_1.TragamonedaSummer(0, 10000, 100);
+var winter1 = new TragamonedaWinter_1.TragamonedaWinter(1, 10000);
+var ruleta1 = new ruleta_1.Ruleta(2, 10000);
+//los dados no necesitan parametro, y alteran el orden.
+var crap1 = new crap_1.Crap(0, 3, 10000);
+//preguntamos la cantidad de plata que posee el cliente.
+function cantDineroCliente() {
+    var readlineSync = require('readline-sync');
+    var cantDineroIngresado = readlineSync.questionInt('Ingrese la cantidad de dinero que va a convertir a credito: ');
+    if (cantDineroIngresado < 1) {
+        console.log("Por favor ingrese un numero valido.");
+        cantDineroCliente();
+    }
+    return cantDineroIngresado;
+}
+var cliente1 = new cliente_1.Cliente(cantDineroCliente());
+//instanciamos la clase Casino.
+var casino777 = new casino_1.Casino(100000, 0.5, cliente1, summer1, winter1, ruleta1, crap1);
+//convertimos todos el dinero del cliente en credito. 
+//esto quedo re feo
+cliente1.setCreditos(casino777.comprarCreditos(cliente1.getDinero()));
+cliente1.setDinero(0);
+cliente1.setCreditos(menuCentral(cliente1.getCreditos()));
+//UNA VEZ TERMINADO DE JUGAR RECUPERAMOS EL DINERO.
+cliente1.setDinero(casino777.intercambiarCreditos(cliente1.getCreditos()));
+//FALTA HACER EL DESCUENTO Y SUMA A LOS JUEGOS.
+function menuCentral(paramCreditos) {
+    console.log("Bienvenido al casino RXXXX");
+    console.log("Usted posee " + paramCreditos + " creditos.");
+    if (paramCreditos > 0) {
+        mensajesMenuCentral();
+        var readlineSync = require('readline-sync');
+        var opcionDeseada = readlineSync.questionInt('Ingrese la opcion deseada: ');
+        if (opcionDeseada < 1 || opcionDeseada > 5) {
+            //REGRESO AL MENU PORQ METISTE LA OPCION INCORRECTA
+            console.log("numero erroneo Intente nuevamente");
+            console.clear(); //para limpiar la pantalla
+            menuCentral(paramCreditos);
+        }
+        else if (opcionDeseada === 5) {
+            //SALIR DEL PROGRAMA CON LA CANTIDAD DE CREDITOS 
+            console.log("Usted se retira con " + paramCreditos + " creditos.");
+            console.log("Gracias por jugar, esperamos su regreso.");
+            return paramCreditos;
+        }
+        else {
+            //EJECUTAR LA OPCION Y REGRESAR AL PROGRAMA CON LA NUEVA CANTIDAD DE CREDITOS
+            menuCentral(ejecucionMaquinas(opcionDeseada, paramCreditos));
+        }
+    }
+    else {
+        console.log("Usted ya no posee creditos suficientes, gracias por jugar vuelva pronto.");
+        return paramCreditos;
     }
 }
-menuTragamonedas(100);
-//console.log("$ ƒ § # + @ 7")
-//ver si se puede cambiar el color de los simbolos
+function mensajesMenuCentral() {
+    console.log("Menu principal");
+    console.log("1 _ Jugar con maquina tragamonedas Summer.");
+    console.log("2 _ Jugar con maquina tragamonedas Winter.");
+    console.log("3 _ Jugar ruleta.");
+    console.log("4 _ Jugar crap.");
+    console.log("5 _ Salir.");
+    console.log("");
+}
+function ejecucionMaquinas(paramOpcion, paramCreditos) {
+    switch (paramOpcion) {
+        case 1:
+            return (0, mainSummer_1.menuSummer)(paramCreditos);
+        case 3:
+            return (0, mainRuleta_1.menuRuleta)(paramCreditos);
+        default:
+            return paramCreditos;
+    }
+}
--- main.ts
+++ main.ts
@@ -3,8 +3,12 @@ import { TragamonedaSummer } from "./casino-poo/tragamonedaSummer";
 import { Cliente } from "./casino-poo/cliente";
 import { menuSummer } from "./mainSummer";
 import { menuRuleta } from "./mainRuleta";
+import { Casino } from "./casino-poo/casino";
+import { TragamonedaWinter } from "./casino-poo/TragamonedaWinter";
+import { Ruleta } from "./casino-poo/ruleta";
+import { Crap } from "./casino-poo/crap";
 
-function menuCentral(paramCreditos: number): void {
+function menuCentral(paramCreditos: number): number {
     console.log("Bienvenido al casino RXXXX");
     console.log("Usted posee " + paramCreditos + " creditos.");
     if(paramCreditos > 0) {
@@ -20,12 +24,14 @@ function menuCentral(paramCreditos: number): void {
             //SALIR DEL PROGRAMA CON LA CANTIDAD DE CREDITOS 
             console.log("Usted se retira con " + paramCreditos + " creditos.");
             console.log("Gracias por jugar, esperamos su regreso.");
+            return paramCreditos;
         } else {
             //EJECUTAR LA OPCION Y REGRESAR AL PROGRAMA CON LA NUEVA CANTIDAD DE CREDITOS
-            menuRuleta(ejecucionMaquinas(opcionDeseada, paramCreditos));
+            menuCentral(ejecucionMaquinas(opcionDeseada, paramCreditos));
         }
     } else {
-        console.log("Usted ya no posee creditos suficientes, gracias por jugar vuelva pronto.")
+        console.log("Usted ya no posee creditos suficientes, gracias por jugar vuelva pronto.");
+        return paramCreditos;
     }
     
 }
@@ -52,3 +58,39 @@ function ejecucionMaquinas(paramOpcion: number, paramCreditos: number): number {
 }
 
 //falta resolver la clase Cliente-Tesorersia-Casino para poder probar esto.
+
+
+let summer1: TragamonedaSummer = new TragamonedaSummer(0, 10000, 100);
+let winter1: TragamonedaWinter = new TragamonedaWinter(1, 10000);
+let ruleta1: Ruleta = new Ruleta(2, 10000);
+//los dados no necesitan parametro, y alteran el orden.
+let crap1: Crap = new Crap(0, 3, 10000);
+
+
+//preguntamos la cantidad de plata que posee el cliente.
+function cantDineroCliente(): number {
+    let readlineSync = require('readline-sync');
+    let cantDineroIngresado: number = readlineSync.questionInt('Ingrese la cantidad de dinero que va a convertir a credito: ');
+    if(cantDineroIngresado < 1 ) {
+        console.log("Por favor ingrese un numero valido.");
+        cantDineroCliente();
+    } 
+    
+    return cantDineroIngresado;
+}
+
+let cliente1: Cliente = new Cliente(cantDineroCliente());
+
+//instanciamos la clase Casino.
+
+let casino777: Casino = new Casino(100000, 0.5, cliente1, summer1, winter1, ruleta1, crap1);
+
+//convertimos todos el dinero del cliente en credito. 
+//esto quedo re feo
+cliente1.setCreditos(casino777.comprarCreditos(cliente1.getDinero()));
+cliente1.setDinero(0);
+cliente1.setCreditos(menuCentral(cliente1.getCreditos()));
+//UNA VEZ TERMINADO DE JUGAR RECUPERAMOS EL DINERO.
+cliente1.setDinero(casino777.intercambiarCreditos(cliente1.getCreditos()))
+
+//FALTA HACER EL DESCUENTO Y SUMA A LOS JUEGOS.
